6c18eb8e84495e7378d0f7329402f7ea4317bf82
ShivaSenaReddy <shivasenajagapathi7@gmail.com>
2022-04-20 11:57:15 +0530

    changes

--- src/Data.js
+++ src/Data.js
@@ -28,13 +28,13 @@ export default [
     },
     {
         title: "Black Adam",
-        releaseDate: "6 May 2021",
+        releaseDate: "21 Oct 2022",
         imgUrl: "https://m.media-amazon.com/images/M/MV5BZDg5ZDg2MWQtM2ExNi00ZjEzLTgzMDQtZmJlYWEwYmM4ODUxXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_.jpg",
         description: "This is a sequel to Dr Strange released in 2016 which has high expections among fans all over the world.",
         trailer: "https://youtu.be/aWzlQ2N6qqg"
     },
     {
-        title: "Jurassic World Dominion",
+        title: "Avatar",
         releaseDate: " 10 June 2022 ",
         imgUrl: "https://m.media-amazon.com/images/M/MV5BMjQzZTA2MmEtNDcwYy00ZWE0LWFlODMtOWQ2MzRjNTgyYmVkXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg",
         description: "This is a sequel to Dr Strange released in 2016 which has high expections among fans all over the world.",
