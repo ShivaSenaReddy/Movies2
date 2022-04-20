2925dabe61b8938914e2617c32fbf20b9a493f58
ShivaSenaReddy <shivasenajagapathi7@gmail.com>
2022-04-20 11:45:04 +0530

    blackadam

--- src/Data.js
+++ src/Data.js
@@ -27,7 +27,7 @@ export default [
        trailer: "https://youtu.be/aWzlQ2N6qqg"
     },
     {
-        title: "Doctor Strange 2",
+        title: "Black Adam",
         releaseDate: "6 May 2021",
         imgUrl: "https://m.media-amazon.com/images/M/MV5BZDg5ZDg2MWQtM2ExNi00ZjEzLTgzMDQtZmJlYWEwYmM4ODUxXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_.jpg",
         description: "This is a sequel to Dr Strange released in 2016 which has high expections among fans all over the world.",
